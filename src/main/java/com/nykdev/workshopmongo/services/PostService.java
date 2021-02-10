8d5f0f53e319ba9ae8df57e4af050e8471a18b6d
nyksonalyson <nyksonalison@hotmail.com>
2021-02-10 01:32:33 -0300

    Consulta com varios criterios

--- src/main/java/com/nykdev/workshopmongo/services/PostService.java
+++ src/main/java/com/nykdev/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.nykdev.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,4 +28,9 @@ import com.nykdev.workshopmongo.services.exception.ObjectNotFoundException;
 			
 		}
 	
+		public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+			maxDate = new Date(maxDate.getTime()+ 24* 60* 60* 1000);
+			return repo.fullSearch(text, minDate, maxDate);
+			
+		}
 	}
